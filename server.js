59bb249f51e0512d7f5cac991f6034d746987538
Alisia-Wegher <aliport2004@gmail.com>
2023-01-04 16:21:26 +0100

    6. scheda giocatore

--- server.js
+++ server.js
@@ -16,30 +16,36 @@ app.get("/giocatori/:club", (req, res)=>{
     let squadra = req.params.club;
     let giocatoriFiltered = giocatori.filter((g)=>{return g.Club.includes(squadra)});
     res.status(200).end(JSON.stringify(giocatoriFiltered));
-    console.log(squadra);
 });
 
 //3. elenco squadre
 app.get("/squadre", (req, res)=>{
-    let squadre = [...new Set(giocatori.map((c)=>c.Club))];
+    let squadre = [...new Set(giocatori.map((g)=>g.Club))];
     res.status(200).end(JSON.stringify(squadre));
 });
 
 //4. elenco nazioni
 app.get("/nazioni", (req, res)=>{
-    let nazioni = [...new Set(giocatori.map((n)=>n.Nazionalita))];
+    let nazioni = [...new Set(giocatori.map((g)=>g.Nazionalita))];
     res.status(200).end(JSON.stringify(nazioni));
 });
 
 //5. elenco posizioni
 app.get("/posizioni", (req, res)=>{
-    let posizioni = [...new Set(giocatori.map((p)=>p.Posizione))];
+    let posizioni = [...new Set(giocatori.map((g)=>g.Posizione))];
     res.status(200).end(JSON.stringify(posizioni));
 });
 
 //6. scheda giocatore
+app.get("/giocatori/scheda/:id", (req, res)=>{
+    let idGiocatore = req.params.id;
+    let giocatoriFiltered = giocatori.filter((g)=>{return g.id==idGiocatore});
+    console.log(idGiocatore);
+    res.status(200).end(JSON.stringify(giocatoriFiltered));
+});
 
 //7. top 10 portieri
+
 //8. età media dei migliori 15 giocatori di una data squadra
 //9. Valore medio dei migliori 15 giocatori di una data squadra
 //10. Elenco dei giocatori di un certo ruolo in ordine di valore crescente
